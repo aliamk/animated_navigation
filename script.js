75228931bd50f11347a3d4ca57516371eb104894
aliamk <alia.azarian@gmail.com>
2020-07-15 21:34:34 +0100

    added a ReadMe and separated the JS scripts from the index.html into their own files

---
+++ script.js
@@ -0,0 +1,7 @@
+
+AOS.init({
+  delay: 200,     // values from 0 - 3000 in 50ms steps
+  duration: 1500, // values from 0 - 3000 in 50ms steps 
+  once: false,    // should animation happen only once while scrolling down
+  mirror: false  // should elements animate out while scrolling past them
+})
